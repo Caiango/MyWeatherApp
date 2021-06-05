8b322b79f2711a8ec93a33be411a5ecc0f5a8cb4
Caiango <cvff12345@gmail.com>
2021-06-05 15:39:32 -0300

    Creating settings Layout

--- app/src/main/java/com/example/myweatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/myweatherapp/ui/settings/SettingsFragment.kt
@@ -1,20 +1,18 @@
-package com.example.myweatherapp.ui.notifications
+package com.example.myweatherapp.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.myweatherapp.R
-import com.example.myweatherapp.databinding.FragmentNotificationsBinding
+import com.example.myweatherapp.databinding.SettingsFragmentBinding
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var settingsViewModel: SettingsViewModel
+    private var _binding: SettingsFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        settingsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = SettingsFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         //val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
             //textView.text = it
         })
         return root
