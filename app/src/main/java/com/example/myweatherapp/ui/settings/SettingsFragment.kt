6aabaa8785cd1c462ba6f427030d2889a6f79cc9
Caiango <cvff12345@gmail.com>
2021-06-05 16:26:12 -0300

    Salvando e recuperando dados shared Pref

--- app/src/main/java/com/example/myweatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/myweatherapp/ui/settings/SettingsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.myweatherapp.ui.settings
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -18,6 +19,8 @@ class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
     private var _binding: SettingsFragmentBinding? = null
+    lateinit var sharedPref: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,6 +30,8 @@ class SettingsFragment : Fragment() {
     private lateinit var radioF: RadioButton
     private lateinit var langP: RadioButton
     private lateinit var langE: RadioButton
+    var temp = ""
+    var lang = ""
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,10 +39,15 @@ class SettingsFragment : Fragment() {
     ): View? {
         settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
-
         _binding = SettingsFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        sharedPref = root.context.getSharedPreferences(
+            getString(R.string.shared_settings), Context.MODE_PRIVATE
+        )
+        editor = sharedPref.edit()
         setupUI(root)
+        checkShared()
 
         //val textView: TextView = binding.textNotifications
         settingsViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -47,38 +57,43 @@ class SettingsFragment : Fragment() {
         return root
     }
 
-    fun setupUI(root: View){
+    private fun checkShared() {
+        if (sharedPref != null) {
+            if (sharedPref.getString("temp", null) == "C") {
+                radioC.isChecked = true
+            } else if (sharedPref.getString("temp", null) == "F"){
+                radioF.isChecked = true
+            }
+            if (sharedPref.getString("lang", null) == "P") {
+                langP.isChecked = true
+            } else if (sharedPref.getString("lang", null) == "E"){
+                langE.isChecked = true
+            }
+        }
+    }
+
+    fun setupUI(root: View) {
         btn = root.findViewById(R.id.btnSave)
+        btn.setOnClickListener { saveData() }
         radioC = root.findViewById(R.id.celsius)
         radioF = root.findViewById(R.id.far)
         langP = root.findViewById(R.id.en)
         langE = root.findViewById(R.id.pt)
-        radioC.setOnClickListener { checkTemp(radioC) }
-        radioF.setOnClickListener { checkTemp(radioF) }
-        langP.setOnClickListener { checkLang(langP) }
-        langE.setOnClickListener { checkLang(langE) }
+
     }
 
-    fun checkTemp(view: View) {
-        when (view.id) {
-            R.id.celsius -> {
-                Toast.makeText(context, "Celcius", Toast.LENGTH_LONG).show()
-            }
-            R.id.far ->{
-                Toast.makeText(context, "Farenheit", Toast.LENGTH_LONG).show()
-            }
+    private fun saveData() {
+        if (radioC.isChecked) temp = "C"
+        if (radioF.isChecked) temp = "F"
+        if (langP.isChecked) lang = "P"
+        if (langE.isChecked) lang = "E"
+        editor.apply {
+            putString("lang", lang)
+            putString("temp", temp)
+            apply()
         }
-    }
+        Toast.makeText(context, "SALVO", Toast.LENGTH_LONG).show()
 
-    fun checkLang(view: View) {
-        when (view.id) {
-            R.id.en -> {
-                Toast.makeText(context, "English", Toast.LENGTH_LONG).show()
-            }
-            R.id.pt ->{
-                Toast.makeText(context, "Portugues", Toast.LENGTH_LONG).show()
-            }
-        }
     }
 
     override fun onDestroyView() {
